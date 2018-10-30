eab88dd3968c81dfd4252f29b5a0d5b94dcfb0b2
svara001 <43080895+svara001@users.noreply.github.com>
2018-10-30 03:56:07 -0400

    update credentials

--- utils/connection.php
+++ utils/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $database = "roomies";
-$username = "root";
-$password = "";
+$username = "admin";
+$password = "monarchs";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
