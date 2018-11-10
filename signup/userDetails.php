d6efffd0923195f570c3636be993385f869e6824
srishti avalakki <42981979+SrishtiAvalakki@users.noreply.github.com>
2018-11-09 20:24:44 -0500

    profile, groups and signup set right

--- signup/userDetails.php
+++ signup/userDetails.php
@@ -14,6 +14,24 @@ if(isset($_POST['submit']))
     }
        if ($conn->query($userQuery) == TRUE ) {
         echo "<script>alert (\"User signed up successfully\")</script>";
+           $verifyemail="SELECT id FROM users WHERE emailId='$_POST[emailid]'";
+    $result=$conn->query($verifyemail);
+    if ($result-> num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            //echo "<script>alert (\"Email Duplicates\")</script>";
+            $res = $row['id'];
+            echo "<script>console.log( 'Debug Objects: " . $res . "' );</script>";
+            $userQuery = "INSERT INTO `usergroupmapping`(`userId`, `groupid`)
+                 VALUES ('$res',1)";
+            $conn->query($userQuery);
+            $userimageQuery = "INSERT INTO `images`(`id`, `image`)
+                 VALUES ('$res','https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi_7aOj_MbeAhXxYt8KHVU-COUQjRx6BAgBEAU&url=https%3A%2F%2Fpngtree.com%2Ffree-icon%2Fthe-default-user_1213197&psig=AOvVaw2DORBZyG3TSPDq28pdunSB&ust=1541841220048327')";
+            $conn->query($userimageQuery);
+    }
+        
+        
+    }
+
     } else {
         ChromePhp::log("Error: " . $userQuery . "<br>" . $conn->error);
     }
