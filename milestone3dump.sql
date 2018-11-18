3940a7a87304a2b40403111318a6c742081e5dc5
svara001 <43080895+svara001@users.noreply.github.com>
2018-11-18 17:29:33 -0500

    change in order of tables

--- milestone3dump.sql
+++ milestone3dump.sql
@@ -31,129 +31,6 @@ grant all privileges on *.* to 'root'@'localhost';
 --
 -- Table structure for table `comments`
 --
-
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `messageId` int(11) NOT NULL,
-  `text` longtext NOT NULL,
-  `sentTime` datetime NOT NULL,
-  `likes` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `comments_fk01` (`messageId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `isPrivate` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`id`, `name`, `isPrivate`) VALUES
-(1, 'Global', 0),
-(2, 'PrivateGroup_Apt1', 1),
-(3, 'PrivateGroup_Apt2', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-DROP TABLE IF EXISTS `messages`;
-CREATE TABLE IF NOT EXISTS `messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `groupId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `text` longtext NOT NULL,
-  `sentTime` datetime NOT NULL,
-  `likes` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `message_fk01` (`groupId`),
-  KEY `message_fk02` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`id`, `groupId`, `userId`, `text`, `sentTime`, `likes`) VALUES
-(1, 1, 1, 'hi', '2018-10-29 19:44:36', 0),
-(2, 1, 2, 'hello', '2018-10-29 09:44:36', 0),
-(3, 2, 1, 'hello', '2018-10-29 19:34:36', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usergroupmapping`
---
-
-DROP TABLE IF EXISTS `usergroupmapping`;
-CREATE TABLE IF NOT EXISTS `usergroupmapping` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `groupId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`userId`,`groupId`),
-  KEY `UserGroupMapping_fk01` (`userId`),
-  KEY `UserGroupMapping_fk02` (`groupId`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `usergroupmapping`
---
-
-INSERT INTO `usergroupmapping` (`id`, `userId`, `groupId`) VALUES
-(12, 0, 1),
-(13, 0, 2),
-(14, 0, 3),
-(1, 1, 1),
-(6, 1, 2),
-(2, 2, 1),
-(7, 2, 2),
-(3, 3, 1),
-(8, 3, 2),
-(11, 3, 3),
-(4, 4, 1),
-(9, 4, 3),
-(5, 5, 1),
-(10, 5, 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usermessagegrouplikes`
---
-
-DROP TABLE IF EXISTS `usermessagegrouplikes`;
-CREATE TABLE IF NOT EXISTS `usermessagegrouplikes` (
-  `userId` int(11) NOT NULL,
-  `messageId` int(11) NOT NULL,
-  PRIMARY KEY (`userId`,`messageId`),
-  KEY `usermessagegrouplikes_fk01` (`userId`),
-  KEY `usermessagegrouplikes_fk02` (`messageId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -188,13 +65,102 @@ INSERT INTO `users` (`id`, `username`, `displayName`, `password`, `emailId`, `ge
 (5, 'Lightning McQueen', 'Lightning McQueen', '@mcqueen', 'kachow@rusteze.com', 'F', '1995-10-18', '', '', '', 0, '', 'Hannible.jpg'),
 (6, 'hello', 'Pagal', 'Srishti@96', 'srishtilakki@gmail.com', 'm', '1996-01-19', 'i am a dancer ', 'watching movies', 'no bio yet ', 762025433, 'bangladesh', 'nibbles_tomandjerry_965.JPG');
 
---
--- Constraints for dumped tables
---
+
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `isPrivate` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Constraints for table `comments`
+-- Dumping data for table `groups`
 --
+
+INSERT INTO `groups` (`id`, `name`, `isPrivate`) VALUES
+(1, 'Global', 0),
+(2, 'PrivateGroup_Apt1', 1),
+(3, 'PrivateGroup_Apt2', 1);
+
+DROP TABLE IF EXISTS `usergroupmapping`;
+CREATE TABLE IF NOT EXISTS `usergroupmapping` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `groupId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`userId`,`groupId`),
+  KEY `UserGroupMapping_fk01` (`userId`),
+  KEY `UserGroupMapping_fk02` (`groupId`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usergroupmapping`
+--
+
+INSERT INTO `usergroupmapping` (`id`, `userId`, `groupId`) VALUES
+(12, 0, 1),
+(13, 0, 2),
+(14, 0, 3),
+(1, 1, 1),
+(6, 1, 2),
+(2, 2, 1),
+(7, 2, 2),
+(3, 3, 1),
+(8, 3, 2),
+(11, 3, 3),
+(4, 4, 1),
+(9, 4, 3),
+(5, 5, 1),
+(10, 5, 3);
+
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `messageId` int(11) NOT NULL,
+  `text` longtext NOT NULL,
+  `sentTime` datetime NOT NULL,
+  `likes` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `comments_fk01` (`messageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `text` longtext NOT NULL,
+  `sentTime` datetime NOT NULL,
+  `likes` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `message_fk01` (`groupId`),
+  KEY `message_fk02` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `groupId`, `userId`, `text`, `sentTime`, `likes`) VALUES
+(1, 1, 1, 'hi', '2018-10-29 19:44:36', 0),
+(2, 1, 2, 'hello', '2018-10-29 09:44:36', 0),
+(3, 2, 1, 'hello', '2018-10-29 19:34:36', 0);
+
+
+DROP TABLE IF EXISTS `usermessagegrouplikes`;
+CREATE TABLE IF NOT EXISTS `usermessagegrouplikes` (
+  `userId` int(11) NOT NULL,
+  `messageId` int(11) NOT NULL,
+  PRIMARY KEY (`userId`,`messageId`),
+  KEY `usermessagegrouplikes_fk01` (`userId`),
+  KEY `usermessagegrouplikes_fk02` (`messageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_fk01` FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`);
 
