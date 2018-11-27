7fea35a54f1a6e69a1b976524d0350af0596d333
Yasha Ravindra <42986064+yasharavindra18@users.noreply.github.com>
2018-11-27 11:26:11 -0500

    login/loginPage.php

--- login/loginPage.php
+++ login/loginPage.php
@@ -1,7 +1,26 @@
 <?php
 session_start();
-if(isset($_POST['submit']))
-{
+//  if(isset($_POST['submit'])){
+    
+  if(isset($_POST['submit'])){
+    if(isset($_POST['g-recaptcha-response'])){
+        $captcha = $_POST['g-recaptcha-response'];
+      }
+      if(!$captcha){
+        echo '<h2>Please check the the captcha form.</h2>';
+        exit;
+      }
+        $secretKey = "6LdKiHwUAAAAAJanuKKjNvd_GJlgH_fDGDrxdSIB";
+      // $secretKey = "6LfJHXwUAAAAAHWUMk_3OZ6UCpkXdfQzyTGMuob2";
+      $ip = $_SERVER['localhost'];
+      $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+      $responseKeys = json_decode($response,true);
+      if(intval($responseKeys["success"]) !== 1) {
+        echo '<h2>You are spammer !</h2>';
+      } else {
+        echo '<h2>Thanks for posting comment.</h2>';
+     
+      }
     require('../utils/connection.php');
     $emailId = mysqli_real_escape_string($conn, $_POST['emailId']); 
     $password = mysqli_real_escape_string($conn, $_POST['password']);
@@ -26,10 +45,12 @@ if(isset($_POST['submit']))
                 header('Location: '.$newURL);
                  exit();
              }
+             
             }
               else {
                 echo "Invalid Password or User does not exist";
             }
-            $conn->close();
+             $conn->close();
+           
 }
 ?>
