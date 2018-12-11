cac90c94c7dcadf7d1f97747b4f5a109166b11c1
svara001 <svara001@odu.edu>
2018-12-10 19:11:08 -0500

    Fixing Admin Redirect Bug

--- checkAccess.php
+++ checkAccess.php
@@ -2,11 +2,8 @@
 session_start();
 
 if(isset($_SESSION['authSatisified']))
-    if ($_SESSION['userid'] == '0') {
-        header('Location: admin.php');
-    }
-    else{
+    if ($_SESSION['userid'] != '0') {
         header('Location: index.php');
     }
-
+    
 ?>
