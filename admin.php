ab00312623ba72b202383d197443f1077aa20b36
Yasha Ravindra <42986064+yasharavindra18@users.noreply.github.com>
2018-12-11 10:47:25 -0500

    favicon addition

--- admin.php
+++ admin.php
@@ -16,6 +16,7 @@ if(!isset($_SESSION['authSatisified'])){
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
     </head>
 <body>
 <div class="topnav" id="myTopnav">
