f3d65f46759bc434e8a13dcb16e523a2ab060409
Yasha Ravindra <42986064+yasharavindra18@users.noreply.github.com>
2018-12-11 11:13:35 -0500

    index.php

--- index.php
+++ index.php
@@ -99,6 +99,7 @@ if(!isset($_SESSION['displayname']) || !isset($_SESSION['userid']) || !isset($_S
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="images/favicon.png" type="image/png"/>
         <title>HomePage</title>
         <style>
             .bio-image{
